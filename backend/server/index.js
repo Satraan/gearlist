d27c5bf722b6b1176978a036d0527b29367b11d0
Satraan <mandiasamuels@gmail.com>
2022-01-25 17:06:19 +0200

    Init

---
+++ backend/server/index.js
@@ -0,0 +1,28 @@
+// server/index.js
+
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const whitelist = ["http://localhost:3002"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
